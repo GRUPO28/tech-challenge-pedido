beadb4b272e629b732157dede879769adecf9681
Eric Silva <ericdss@hotmail.com>
2024-12-01 16:42:02 -0300

    Adicionar suporte ao MySQL para persistir pagamentos

--- tests/Modules/Pedido/Pedidos.Pedido.Test.Unit/Domain/Entities/PagamentoTest.cs
+++ tests/Modules/Pedido/Pedidos.Pedido.Test.Unit/Domain/Entities/PagamentoTest.cs
@@ -7,7 +7,7 @@ public class PagamentoTest : BaseUnitTest
     [Fact]
     public void Validate_Should_Pass_When_PagamentoValido()
     {
-        Action act = () => new Pagamento(null!, DateTime.UtcNow, DateTime.UtcNow.AddMinutes(1), GerarIdValido(), "https://mercadolivre.com/fake-url");
-        Assert.NotNull(act);
+        var pagamento = new Pagamento(null!, DateTime.UtcNow, DateTime.UtcNow.AddMinutes(1), GerarIdValido(), "https://mercadolivre.com/fake-url");
+        Assert.NotNull(pagamento);
     }
 }
